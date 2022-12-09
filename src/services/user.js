568749e893b75532d3cb85a8e2c9fe14d4926a40
Artyom Sankov <artemsankov22@gmail.com>
2022-12-09 22:25:14 +0300

    add services user

--- src/services/user.js
+++ src/services/user.js
@@ -4,9 +4,28 @@ const instance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
 
-const loginUser = () => {
-  instance.post("/user/login", {
-    username: username,
-    password: password,
+instance.interceptors.request.use((config) => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  return config;
+});
+
+const loginUser = async () => {
+  return instance.post("/user/login", {
+    username,
+    password,
+  });
+};
+
+const registerUser = async () => {
+  return instance.post("/user/register", {
+    username,
+    password,
+  });
+};
+
+const logout = async (id) => {
+  return instance.post(`/user/delete/${id}`, {
+    username,
+    password,
   });
 };
